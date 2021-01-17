14ca840bd7563ea02a4f49d3a0660258d3862525
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-01-16 22:28:54 -0600

    Commit lesson 1 branch before hw

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 37},
+            new Employee {Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27},
+            new Employee {Id = 3, LastName = "Васечкин", FirstName = "Василий", Patronymic = "Васильевич", Age = 25}
+        };
+        public IActionResult Index() => View();
+ 
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
+        }
+    }
+}
