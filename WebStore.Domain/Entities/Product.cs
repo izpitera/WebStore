e7edcaf27ce3176a3829f81ae6fff6d1e62e68fa
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-02-01 22:38:55 -0600

    db context

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -21,6 +21,7 @@ namespace WebStore.Domain.Entities
         [ForeignKey(nameof(BrandId))]
         public Brand Brand { get; set; }
         public string ImageUrl { get; set; }
+
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
