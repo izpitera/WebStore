01a534bf476b6140a01375d9094b1d345e9b0150
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-02-07 19:38:59 -0600

    user registration

---
+++ WebStore/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class RegisterUserViewModel
+    {   
+        [Required, MaxLength(256)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Password Confirmation")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; set; }
+    }
+}
