84aecafae7fabdfc91fbf0e995320e254a1e1d0e
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-02-07 21:16:57 -0600

    hw6: Identity

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels;
 
@@ -13,20 +12,25 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _UserManager;
         private readonly SignInManager<User> _SignInManager;
+        private readonly ILogger<AccountController> _Logger;
 
-        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager, ILogger<AccountController> Logger)
         {
             _UserManager = UserManager;
             _SignInManager = SignInManager;
+            _Logger = Logger;
         }
 
         #region Register
+
         public IActionResult Register() => View(new RegisterUserViewModel());
 
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterUserViewModel Model)
         {
             if (!ModelState.IsValid) return View(Model);
+
+            _Logger.LogInformation("Register user {0}", Model.UserName);
 
             var user = new User
             {
@@ -36,21 +40,29 @@ namespace WebStore.Controllers
             var registration_result = await _UserManager.CreateAsync(user, Model.Password);
             if (registration_result.Succeeded)
             {
+                _Logger.LogInformation("User {0} registered successfully", Model.UserName);
+
                 await _SignInManager.SignInAsync(user, false);
 
                 return RedirectToAction("Index", "Home");
             }
+
+            _Logger.LogWarning("User {0} registration process failed with errors: {1}", 
+                Model.UserName, 
+                string.Join(',', registration_result.Errors.Select(e => e.Description))
+                );
 
             foreach (var error in registration_result.Errors)
                 ModelState.AddModelError("", error.Description);
 
             return View(Model);
         }
+
         #endregion
 
         #region Login
 
-        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl});
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel {ReturnUrl = ReturnUrl});
 
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel Model)
@@ -63,7 +75,7 @@ namespace WebStore.Controllers
                 Model.RememberMe,
 #if DEBUG
                 false
-#else 
+#else
                 true
 #endif
             );
@@ -72,7 +84,7 @@ namespace WebStore.Controllers
             {
                 return LocalRedirect(Model.ReturnUrl ?? "/");
             }
-            
+
             ModelState.AddModelError("", "Wrong User Name or Password");
 
             return View(Model);
@@ -81,6 +93,16 @@ namespace WebStore.Controllers
 
         #endregion
 
+        public async Task<IActionResult> Logout()
+        {
+            await _SignInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
+        public IActionResult AccessDenied(string ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        } 
     }
 }
