925c86690799ef831ae2ee57bf9185cbc4076b2e
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-02-14 22:42:45 -0600

    Db migration, orders added

--- WebStore.Domain/Entities/Orders/Order.cs
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -13,22 +13,22 @@ namespace WebStore.Domain.Entities.Orders
     public class Order : NamedEntity
     {
         [Required]
-        public User User { get; set; }
+        public virtual User User { get; set; }
         [Required]
         public string Phone { get; set; }
         [Required]
         public string Address { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
 
-        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public virtual ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
     }
 
     public class OrderItem : Entity
     {
         [Required]
-        public Order Order { get; set; }
+        public virtual Order Order { get; set; }
         [Required]
-        public Product Product { get; set; }
+        public virtual Product Product { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public int Quantity { get; set; }
