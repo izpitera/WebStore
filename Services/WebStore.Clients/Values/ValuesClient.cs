1529824db52470522b5367caa820a4d790900bb7
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-04-18 19:43:33 -0500

    BaseClient

---
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Clients.Base;
+
+namespace WebStore.Clients.Values
+{
+    public class ValuesClient: BaseClient
+    {
+    }
+}
