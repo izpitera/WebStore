d7aa583749ab8470037af490a658d851bb76fbd2
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-04-18 19:20:36 -0500

    Test service

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -15,10 +15,7 @@ namespace WebStore.ServiceHosting
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
