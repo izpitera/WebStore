9262f9396283dee6cacd6a5315503e9febdf241e
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-04-21 21:57:04 -0500

    WebApi Client for employees

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -13,33 +14,16 @@ namespace WebStore.Clients.Employees
 {
     public class EmployeesClient: BaseClient, IEmployeesData
     {
-        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees)
-        {
-        }
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees) { }
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
 
-        public Employee Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
 
-        public int Add(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
 
-        public void Update(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public void Update(Employee employee) => Put(Address, employee);
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
     }
 }
