b9616d458c9364079116be55c13cde0384f0589e
KonstantinKonovalov <k_konovalov@yahoo.com>
2021-04-21 21:02:20 -0500

    WebApi Controller for employees

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet]
+        public IEnumerable<Employee> Get() => _EmployeesData.Get();
+
+        [HttpGet("{id}")]
+        public Employee Get(int id) => _EmployeesData.Get(id);
+
+        [HttpPost]
+        public int Add(Employee employee) => _EmployeesData.Add(employee);
+
+        [HttpPut]
+        public void Update(Employee employee) => _EmployeesData.Update(employee);
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+    }
+}
